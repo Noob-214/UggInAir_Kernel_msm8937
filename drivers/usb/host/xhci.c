d1205433664279a3d01efeaad2befb15c1696926
Noob-214 <66304558+Noob-214@users.noreply.github.com>
2020-08-01 02:32:44 -0400

    usb: host: removed unusal codes

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -3840,15 +3840,13 @@ static int xhci_setup_device(struct usb_hcd *hcd, struct usb_device *udev,
 	u64 temp_64;
 	struct xhci_command *command = NULL;
 
-<<<<<<< HEAD
 	mutex_lock(&xhci->mutex);
 
 	if (xhci->xhc_state)	/* dying, removing or halted */
 		goto out;
-=======
+
 	if (xhci->xhc_state)	/* dying, removing or halted */
 		return -EINVAL;
->>>>>>> e04b898d9450e695141c1d06b59009973701c184
 
 	if (!udev->slot_id) {
 		xhci_dbg_trace(xhci, trace_xhci_dbg_address,
